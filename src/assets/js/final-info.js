17ea100e0fe6ca05bd5b601afd7f50606106548e
makkez <mailto:metallmaster@bk.ru>
2021-10-06 23:32:39 +0300

    code

---
+++ src/assets/js/final-info.js
@@ -0,0 +1,4 @@
+var pFinalInfoElem = document.getElementById('final-info');
+if (pFinalInfoElem !== undefined) {
+    pFinalInfoElem.innerText = '© “Авторесурс” 2005 - ' + new Date().getFullYear();
+}
