d33b1b6bc428e718280082cdaa93931c64ce8213
makkez <mailto:metallmaster@bk.ru>
2021-10-17 02:25:01 +0300

    sending email

--- src/php/sendmail.php
+++ src/php/sendmail.php
@@ -1,24 +1,62 @@
 <?php
+
+    require __DIR__.'/vendor/PHPMailer/PHPMailer.php';
+    require __DIR__.'/vendor/PHPMailer/SMTP.php';
+    require __DIR__.'/vendor/PHPMailer/Exception.php';
+
     use PHPMailer\PHPMailer\PHPMailer;
     use PHPMailer\PHPMailer\Exception;
+    use PHPMailer\PHPMailer\SMTP;
 
-    require 'phpmailer/src/Exception.php';
-    require 'phpmailer/src/PHPMailer.php';
+    $phone = trim($_POST['phone']);
+
+    $title = 'Заявка обратной связи';
+    $body = "
+    <p>Здравствуйте!</p>
+    <p>Прошу связаться со мной по номеру: 
+    <b> $phone</b>.</p>
+    <p>Спасибо!</p>
+    ";
 
     $mail = new PHPMailer(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->setLanguage('ru', 'phpmailer/language/');
-    $mail->IsHTML(true);
+    try {
+        $mail->isSMTP();
+        $mail->CharSet = 'UTF-8';
+        $mail->SMTPAuth = true;
+        $mail->SMTPDebug = 2;
+        $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
 
-    $mail->setFrom('metallmaster@bk.ru', 'ROBOT');
-    $mail->addAddress('mkezikov@yandex.ru');
-    $mail->Subject = 'Заявка обратной связи';
+        $mail->Host = 'smtp.mail.ru';
+        $mail->Username = 'metallmaster@bk.ru';
+        $mail->Password = '40ivFuU4LZrjABCiiP3w';
+        $mail->SMTPSecure = 'ssl';
+        $mail->Port = 465;
+        $mail->setFrom('metallmaster@bk.ru', 'Имя отправителя');
+		
+        /*
+		$mail->smtpConnect(
+			array(
+				"ssl" => array(
+					"verify_peer" => false,
+					"verify_peer_name" => false,
+					"allow_self_signed" => true
+				)
+			)
+		);
+        */
 
-    $body = '<p>Здравствуйте!</p>'
+        $mail->addAddress('mkezikov@yandex.ru');
 
-    if (trim(!empty($_POST['phone']))) {
-        $body.= '<p>Прошу связаться со мной по номеру: <b>'.$_POST['phone'].'</b>.</p><p>Спасибо!</p>';
+        $mail->isHTML(true);
+        $mail->Subject = $title;
+        $mail->Body = $body;
+
+        if ($mail->send()) { $result = "success"; } 
+        else { $result = "error"; }
+    } catch (Exception $e) {
+        $result = "error";
+        $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
     }
 
-    $mail->Body = $body;
+    echo json_encode(["result" => $result, "resultfile" => $rfile, "status" => $status]);
 ?>
